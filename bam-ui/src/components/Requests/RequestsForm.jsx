90ee6f85786fdabf93bd3bda4b37a39057c7d569
Shelly <shelysaraev@gmail.com>
2025-06-11 15:54:59 +0300

    Class name change

--- bam-ui/src/components/Requests/RequestsForm.jsx
+++ bam-ui/src/components/Requests/RequestsForm.jsx
@@ -24,37 +24,44 @@ const RequestsForm = () => {
     // optional: Add validation
     const body = {
       description: enteredDescription,
-      explanation: enteredExplanation, 
+      explanation: enteredExplanation,
       status: {
         state: "pending",
       },
     };
-    sendUserRequest({
-      url: "http://localhost:5000/requests",
-      method: "POST",
-      headers: {
-        Authorization: authCtx.token,
-        "Content-Type": "application/json",
+    sendUserRequest(
+      {
+        url: "http://localhost:5000/requests",
+        method: "POST",
+        headers: {
+          Authorization: authCtx.token,
+          "Content-Type": "application/json",
+        },
+        body,
       },
-      body,
-    }, () => {
+      () => {
         descriptionInputRef.current.value = "";
         explanationInputRef.current.value = "";
-    });
+      }
+    );
   };
 
   return (
-    <section className={classes.auth}>
+    <section className={classes.content}>
       <h1>בקשה חדשה</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="description">סוג הבקשה</label>
           <select id="description" required ref={descriptionInputRef}>
-            <option value=" "> </option>
+            <option value=""> </option>
             <option value="בקשת השחרה">בקשת השחרה</option>
-            <option value="בקשת אישור כניסה רגלי/רכוב לבהד">בקשת אישור כניסה רגלי/רכוב לבה"ד</option>
+            <option value='בקשת אישור כניסה רגלי/רכוב לבה"ד'>
+              בקשת אישור כניסה רגלי/רכוב לבה"ד
+            </option>
             <option value="בקשת קידוד חוגר">בקשת קידוד חוגר</option>
-            <option value="בקשת טופס חתימה על שוס">בקשת טופס חתימה על שו"ס</option>
+            <option value='בקשת טופס חתימה על שו"ס'>
+              בקשת טופס חתימה על שו"ס
+            </option>
           </select>
         </div>
         <div className={classes.control}>
