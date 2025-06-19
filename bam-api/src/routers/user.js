b99bb9cf9591e1a0196d0a44778912f26cead340
Shelly <shelysaraev@gmail.com>
2025-06-19 23:11:46 +0300

    Changed reset-password endpoint & added nodemailer functions

--- bam-api/src/routers/user.js
+++ bam-api/src/routers/user.js
@@ -2,8 +2,11 @@ const express = require("express");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const router = new express.Router();
 const CODES = require("../utils/status-codes");
+
+const nodemailer = require("nodemailer");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -57,7 +60,33 @@ router.post("/users/forgot-password", async (req, res) => {
       { expiresIn: "5m" }
     );
     const link = `${process.env.HOST}/users/reset-password/${existingUser._id}/${token}`;
+
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    let mailOptions = {
+      from: "youremail@gmail.com",
+      to: process.env.EMAIL_USER,
+      subject: "Password Reset",
+      text: link,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      console.log("hi");
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+
     console.log(link);
+    res.send(link);
   } catch (e) {
     res.status(CODES.NOT_FOUND).send();
   }
@@ -73,29 +102,39 @@ router.get("/users/reset-password/:id/:token", async (req, res) => {
   const secret = process.env.JWT_SECRET + existingUser.password;
   try {
     const verify = jwt.verify(token, secret);
-    res.send("Verified!");
+    res.send({ email: verify.email });
   } catch (e) {
     res.send("Not Verified!");
   }
 });
 
-router.patch("/users/update-password", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["password"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(CODES.BAD_REQUEST).send({ error: "Invalid updates!" });
+router.post("/users/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+  console.log(req.params);
+  const existingUser = await User.findOne({ _id: id });
+  if (!existingUser) {
+    return res.json({ status: "User Not Exists!" });
   }
-
+  const secret = process.env.JWT_SECRET + existingUser.password;
   try {
-    updates.forEach((update) => (req.user[update] = req.body[update]));
-    await req.user.save();
-    res.send(req.user);
+    const verify = jwt.verify(token, secret);
+    const encryptedPassword = await bcrypt.hash(password, 8);
+    await User.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          password: encryptedPassword,
+        },
+      }
+    );
+
+    // res.send({ email: verify.email });
+    res.json({ status: "Password Updated!" });
   } catch (e) {
-    res.status(CODES.BAD_REQUEST).send();
+    res.json({ status: "Something Went Wrong!" });
   }
 });
 
