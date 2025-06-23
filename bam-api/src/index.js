e73a423cdd3ed7399517a7c34dc39a77513b3c86
Shelly <shelysaraev@gmail.com>
2025-06-23 13:48:08 +0300

    Installed socket.io

--- bam-api/src/index.js
+++ bam-api/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require('dotenv').config();
 const cors = require("cors");
-const app = express();
 require("./db/mongoose");
+const socketio = require("socket.io");
 const userRouter = require("./routers/user");
 const requestRouter = require("./routers/request");
 const port = process.env.PORT;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
